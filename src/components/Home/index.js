c2859d017c9fbe2a8a55a42641e0f14ed9ffc224
Gatla Raviteja <121756394+Ravi30T@users.noreply.github.com>
2024-04-07 21:01:20 +0530

    Unable to view Output

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,18 +4,35 @@ import {Component} from 'react'
 
 import GetMessage from '../Message'
 
+import Login from '../Login'
+
+import Logout from '../Logout'
+
 class Home extends Component {
   state = {login: false}
 
-  render() {
-    // const {message} = this.props
+  onLogin = () => {
+    this.setState({login: true})
+  }
 
+  onLogout = () => {
+    this.setState({login: false})
+  }
+
+  render() {
     const {login} = this.state
 
     return (
       <div className="bgContainer">
         <div className="cardContainer">
           <GetMessage className={login} />
+          <div>
+            {login ? (
+              <Logout onClickLogout={this.onLogout} />
+            ) : (
+              <Login onClickLogin={this.onLogin} />
+            )}
+          </div>
         </div>
       </div>
     )
